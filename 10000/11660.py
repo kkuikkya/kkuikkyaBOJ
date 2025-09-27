2ba4c8e54664513a1b0f8332da05780bb650f0da
kkuikkya <kkuikkya@gmail.com>
2025-09-27 14:53:20 +0000

    250927 / 11660

---
+++ 10000/11660.py
@@ -0,0 +1,22 @@
+# 11660: 구간합 구하긱5
+
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+
+arr = []
+dp = [[0 for _ in range(N+1)] for _ in range(N+1)]
+
+for _ in range(N):
+    arr.append(list(map(int, sys.stdin.readline().split())))
+
+
+# dp
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + arr[i-1][j-1]
+
+for _ in range(M):
+    x1, y1, x2, y2 = map(int, sys.stdin.readline().split())
+
+    print(dp[x2][y2] - dp[x2][y1-1] - dp[x1 -1][y2] + dp[x1-1][y1-1])
